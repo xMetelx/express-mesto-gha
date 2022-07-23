3fe6f5bfe2ea83187a6f66c4676e24d7198c455d
xMetelx <xmetelx@yandex.ru>
2022-07-23 22:03:33 +0300

    errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,37 +5,37 @@ module.exports.getCards = (req, res) => {
     .populate(['owner', 'likes'])
     .then((cards) => {
       if (!cards) {
-        res.status(400).send('Переданы некорректные данные при создании карточки');
+        res.status(400).send({ message: 'Переданы некорректные данные при создании карточки' });
         return;
       }
       res.status(200).send(cards);
     })
-    .catch(() => res.status(500).send('Ошибка по умолчанию')); // Ошибка сервера
+    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' })); // Ошибка сервера
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => {
-      // if (!card) {
-      //   res.status(400).send('Переданы некорректные данные при создании карточки');
-      //   return;
-      // }
-      res.status(200).send(card);
-    })
-    .catch((err) => res.status(500).send(err.message)); // Ошибка сервера
+    .then((card) => { res.status(200).send(card); })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при создании карточки' });
+        return;
+      }
+      res.status(500).send({ message: 'Ошибка по умолчанию' });
+    }); // Ошибка сервера
 };
 
 module.exports.deleteCard = (req, res) => Card.findByIdAndRemove(
   req.params.cardId,
   (err) => {
     if (err) {
-      res.status(404).send('Карточка с указанным _id не найдена');
+      res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
     }
   },
 )
   .then((card) => res.status(200).send(card))
-  .catch(() => res.status(500).send('Ошибка по умолчанию'));
+  .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }));
 
 module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   { _id: req.params.cardId },
@@ -43,14 +43,14 @@ module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   { new: true },
   (err) => {
     if (err) {
-      res.status(404).send('Карточка с указанным _id не найдена');
+      res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
     }
   },
 )
   .then((card) => {
     res.status(200).send(card);
   })
-  .catch(() => res.status(500).send('Ошибка по умолчанию1'));
+  .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }));
 
 module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
@@ -60,4 +60,4 @@ module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   .then((card) => {
     res.status(200).send(card);
   })
-  .catch(() => res.status(500).send('Ошибка по умолчанию'));
+  .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }));
--- controllers/users.js
+++ controllers/users.js
@@ -4,12 +4,12 @@ module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => {
       if (!users) {
-        res.status(400).send('Переданы некорректные данные при создании пользователя');
+        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
         return;
       }
       res.status(200).send(users);
     })
-    .catch(() => res.status(500).send('Ошибка по умолчанию')); // Ошибка сервера
+    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' })); // Ошибка сервера
 };
 
 module.exports.getUserById = (req, res) => {
@@ -17,24 +17,25 @@ module.exports.getUserById = (req, res) => {
   User.findById(id)
     .then((user) => {
       if (!user) {
-        res.status(404).send('Пользователь с указанным _id не найден');
+        res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
         return;
       }
       res.status(200).send(user);
     })
-    .catch((err) => res.status(500).send('Ошибка по умолчанию')); // Ошибка сервера
+    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' })); // Ошибка сервера
 };
 
 module.exports.createUser = (req, res) => {
   User.create({ ...req.body })
-    .then((user) => { res.status(200).send(user) })
+    .then((user) => { res.status(200).send(user); })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(400).send('Переданы некорректные данные при создании пользователя');
+        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
+        return;
       }
-      res.status(500).send('Ошибка по умолчанию')})
-}
-
+      res.status(500).send({ message: 'Ошибка по умолчанию' });
+    });
+};
 
 module.exports.patchProfile = (req, res) => {
   User.findByIdAndUpdate(
@@ -45,18 +46,18 @@ module.exports.patchProfile = (req, res) => {
     },
     (err) => {
       if (err) {
-        res.status(404).send('Пользователь с указанным _id не найден');
+        res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
       }
     },
   )
     .then((user) => {
       if (!user) {
-        res.status(400).send('Переданы некорректные данные при обновлении профиля');
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
         return;
       }
       res.status(200).send(user);
     })
-    .catch(() => res.status(500).send('Ошибка по умолчанию'));
+    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }));
 };
 
 module.exports.patchAvatar = (req, res) => {
@@ -66,10 +67,10 @@ module.exports.patchAvatar = (req, res) => {
   )
     .then((user) => {
       if (!user) {
-        res.status(400).send('Переданы некорректные данные при обновлении аватара');
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара' });
         return;
       }
       res.status(200).send({ avatar: user.avatar });
     })
-    .catch(() => res.status(500).send('Ошибка по умолчанию'));
+    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }));
 };
