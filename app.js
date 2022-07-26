9f13af2552a9a3d8dd85e33af8af258b7d142e80
xMetelx <xmetelx@yandex.ru>
2022-07-27 01:13:41 +0300

    errors11

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ app.use((req, res, next) => {
 
 app.use('/cards', cardRouter);
 app.use('/users', userRouter);
-app.patch('/404', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемой страницы не существует' });
-});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
